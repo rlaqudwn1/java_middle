edd91c21d6d48b03c3d6e865e8d2e8ff380474a7
rlaqudwn1 <rlaqudwn123456@gmail.com>
2024-04-13 14:59:39 +0900

    javaLearn

---
+++ src/src/execption/basic/unchecked/Client.java
@@ -0,0 +1,11 @@
+package execption.basic.unchecked;
+
+
+
+public class Client {
+    public void call() {
+        throw new MyUncheckedException("ex");
+
+    }
+
+}
---
+++ src/src/execption/basic/unchecked/UnCheckedCatchMain.java
@@ -0,0 +1,9 @@
+package execption.basic.unchecked;
+
+public class UnCheckedCatchMain {
+    public static void main(String[] args) {
+        Service service = new Service();
+        service.callCatch();
+        System.out.println("정상 종료");
+    }
+}
