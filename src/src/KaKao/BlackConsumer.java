73a86a6797f720bb6b00b7bf9d42c59098155131
rlaqudwn1 <rlaqudwn123456@gmail.com>
2024-05-27 10:07:25 +0900

    coding_test Learn

---
+++ src/src/KaKao/BlackConsumer.java
@@ -0,0 +1,22 @@
+package KaKao;
+
+/**
+ * 1. banned_id.lenth 와 같은 id를 선별하고
+ * 2. user_id 에서 banned_id *에 해당하는 좌표값을 *로 치환한다.
+ * 3. uesr_id와 banned_id가 같을 경우 배열에 추가한다.
+ * 4. 배열 길이가 1인 id의 값과 중복되는 값을 삭제한다.
+ * 4-1 ex) while(true){
+ *     if(id.equals(user.id)){
+ *         while =true;
+ *     }else while=false;
+ * }
+ */
+public class BlackConsumer {
+    public int solution(String[] user_id, String[] banned_id) {
+        int answer = 0;
+        return answer;
+    }
+    public static void main(String[] args){
+
+    }
+}
--- src/src/KaKao/Delivery.java
+++ src/src/KaKao/Delivery.java
@@ -6,33 +6,39 @@ package KaKao;
  * 3. 배달한 곳과 수거한 곳의 배열을 0으로 제거하며 뒤에서 앞으로 1,2의 과정을 반복한다.
  * 4. 배달 거리는 맨 마지막 배달집 기준 2배로 잡는다.**
  */
-public class Delivery { public long solution(int cap, int n, int[] deliveries, int[] pickups) {
-    long answer = -1;
-    boolean index= true;
-    int len=0;
+public class Delivery {
+    public long solution(int cap, int n, int[] deliveries, int[] pickups) {
+    long answer = 0;
+    int len = 0;
     int last_index = deliveries.length;
-    while (true) {
-        index=true;
-        int caps= cap;
-        for (int i = deliveries.length-1; i >= 0; i++) {
-            if (deliveries[i] !=0) {
-                if (cap < deliveries[i]) {
-                    break;
-                } else {
-                    caps -= deliveries[i];
-                    deliveries[i]=0;
-                }
+        for (int i = deliveries.length - 1; i >= 0; ) {
+            if (deliveries[i] == 0 && pickups[i] == 0) {
+                i--;
+                continue;
             }
-
-
-                if (index) {
-                    len=i;
-                    index=false;
+            for (int j = i; j >= 0; ) {
+                if (cap >= deliveries[j]) {
+                    cap -= deliveries[j];
+                    deliveries[j--] = 0;
+                } else {
+                    deliveries[j] -= cap;
+                    break;
                 }
+                for (int k = i; k >= 0; ) {
+                    if (cap >= pickups[k]) {
+                        cap -= pickups[k];
+                        deliveries[k--] = 0;
+                    } else {
+                        deliveries[k] -= cap;
+                        break;
+                    }
+                }
+                answer += (long)((i + 1) * 2);
             }
         }
-//    return answer;
-}
+
+    return answer;
+    }
 
     public static void main(String[] args) {
         Delivery delivery = new Delivery();
